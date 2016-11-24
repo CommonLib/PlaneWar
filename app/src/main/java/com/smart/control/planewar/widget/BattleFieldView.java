052466dac91c7f8f1f58b840db9f3706d3a05c16
burt.yang <burt.yang@pwc.com>
2016-11-24 17:43:08 +0800

    submit code

--- app/src/main/java/com/smart/control/planewar/widget/BattleFieldView.java
+++ app/src/main/java/com/smart/control/planewar/widget/BattleFieldView.java
@@ -1,17 +1,35 @@
 package com.smart.control.planewar.widget;
 
 import android.content.Context;
-import android.widget.FrameLayout;
+import android.graphics.Canvas;
+
+import java.util.ArrayList;
 
 /**
  * Created by byang059 on 11/24/16.
+ * 存在子弹 敌机 弹药，根据他们的坐标进行绘制
  */
 
-public class BattleFieldView extends FrameLayout {
+public class BattleFieldView extends Element {
+    public ArrayList<Element> mElements = new ArrayList<>();
 
     public BattleFieldView(Context context) {
         super(context);
     }
 
+    public void drawElement(Element element){
+        if(mElements.contains(element)){
+            return;
+        }
+        mElements.add(element);
+    }
 
+    public void removeElememt(Element element){
+        mElements.remove(element);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+    }
 }
